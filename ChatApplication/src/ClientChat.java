6b1b2eeb3c06a26f4403f2badc1a9ff3ce4ccde9
AGS Lakpahana <agslakpahana@gmail.com>
2024-02-16 10:02:43 +0530

    WorkShop 1 ChatApp

---
+++ ChatApplication/src/ClientChat.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.Buffer;
+
+public class ClientChat {
+    static int port = 6600;
+    public static void main(String[] args) throws IOException {
+        System.out.println("\\t\\t Simple Socket Client");
+        System.out.println("\\t\\t =================== \n\n");
+
+        InetAddress ipAddress = InetAddress.getLocalHost();
+
+        Socket socket = new Socket(
+                ipAddress,
+                port
+        );
+
+        System.out.println("Server is connected........\n\n");
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream())
+        );
+
+        String message = in.readLine();
+
+        System.out.println("Server says:"+ message);
+
+        System.out.println("Closing the connection");
+
+        socket.close();
+
+        in.close();
+
+        System.exit(0);
+    }
+}
---
+++ ChatApplication/src/ServerChat.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerChat {
+    static int port = 6600;
+    public static void main (String[] args) throws IOException {
+
+        System.out.println("\t\t Simple Socket Server");
+        System.out.println("\t\t =================== \n\n");
+
+
+        ServerSocket serverSocket =  new ServerSocket(port);
+
+        try {
+            while(true){
+
+                Socket socket = serverSocket.accept();
+
+
+
+                System.out.println("Client is connected........\n\n\n");
+
+                try{
+
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+
+                    out.println("Hello Client, I am the server, welcome to socket programming");
+
+                }finally {
+                    socket.close();
+                }
+
+
+
+            }
+        }finally {
+            serverSocket.close();
+        }
+
+
+
+
+
+
+    }
+}
